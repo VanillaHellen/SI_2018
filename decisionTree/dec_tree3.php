44aee89f820540b3cbbbb7ae5272a775e28e0a1b
Karol Piekarski <karol@codeaddict.io>
2018-06-06 23:16:40 +0200

    add decisionTree

---
+++ decisionTree/dec_tree3.php
@@ -0,0 +1,14 @@
+<?php
+include 'DecisionTree.php';
+include 'Node.php';
+
+//error_reporting(E_ALL | E_STRICT);
+
+echo "Using training data to generate Decision tree...\n";
+$dec_tree = new DecisionTree('data.csv', 0);
+echo "Decision tree using ID3:\n";
+$dec_tree->display();
+echo "Prediction on new data set\n";
+$dec_tree->predict_outcome('input_data.csv');
+exit();
+
