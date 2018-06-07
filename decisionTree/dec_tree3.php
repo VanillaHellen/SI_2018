00654fcf8df31a3b573bd4eb9e4aeab7af49f68c
Karol Piekarski <karol@codeaddict.io>
2018-06-07 16:50:00 +0200

    stage to show

--- decisionTree/dec_tree3.php
+++ decisionTree/dec_tree3.php
@@ -5,10 +5,10 @@ include 'Node.php';
 //error_reporting(E_ALL | E_STRICT);
 
 echo "Using training data to generate Decision tree...\n";
-$dec_tree = new DecisionTree('data.csv', 0);
-echo "Decision tree using ID3:\n";
+$dec_tree = new DecisionTree('data.csv', 1);
+//echo "Decision tree using ID3:\n";
 $dec_tree->display();
-echo "Prediction on new data set\n";
-$dec_tree->predict_outcome('input_data.csv');
+//echo "Prediction on new data set\n";
+//$dec_tree->predict_outcome('input_data.csv');
 exit();
 
